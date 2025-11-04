fe34f96a029ca18c7ef632976fbd9eff74cf435b
안상아 <dkstkddkdhkd@naver.com>
2025-11-04 16:40:42 +0900

    E11

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <E10Ref />
+  <E11Reactive />
 </template>
 
 <script setup lang="ts">
@@ -7,6 +7,7 @@ import E07OptionsAPI from "./components/example5/E-07-Options-API.vue";
 import E08CompositionApi from "./components/example5/E-08-composition-api.vue";
 import E09CompositionAPI2 from "./components/example5/E-09-composition-API2.vue";
 import E10Ref from "./components/example6/E-10-ref.vue";
+import E11Reactive from "./components/example6/E-11-reactive.vue";
 </script>
 
 <style>
