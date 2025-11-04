321cd40ec2c5e73243ea918f254a3eb00e15aaa2
안상아 <dkstkddkdhkd@naver.com>
2025-11-04 16:39:42 +0900

    E10

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,12 @@
 <template>
-  <E09CompositionAPI2 />
+  <E10Ref />
 </template>
 
 <script setup lang="ts">
 import E07OptionsAPI from "./components/example5/E-07-Options-API.vue";
 import E08CompositionApi from "./components/example5/E-08-composition-api.vue";
 import E09CompositionAPI2 from "./components/example5/E-09-composition-API2.vue";
+import E10Ref from "./components/example6/E-10-ref.vue";
 </script>
 
 <style>
